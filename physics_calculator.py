294e7bfec85fd3667b6464191e999aa84a2c7976
suliman <sulimanawadstudy@gmail.com>
2022-05-15 03:55:17 +0300

    get the first demo enviroment.

--- physics_calculator.py
+++ physics_calculator.py
@@ -4,16 +4,16 @@ from vector import *
 g = 6.6743 * (10**-11)
 
 
-def calc_a(m: float, f: Vector) -> Vector:
+def calc_acceleration(m: float, f: Vector) -> Vector:
     return f/m
 
 
-def calc_v(v: Vector, a: Vector, dt: float) -> Vector:
+def calc_velocity(v: Vector, a: Vector, dt: float) -> Vector:
     return v + a*dt
 
 
-def calc_p(p: Vector, v: Vector, a: Vector, dt: float) -> Vector:
-    return p + (v*dt) + (a*((dt**2)/2))
+def calc_pos(pos: Vector, v: Vector, a: Vector, dt: float) -> Vector:
+    return pos + (v*dt) + (a*((dt**2)/2))
 
 
 def calc_momentum(m: float, v: Vector) -> Vector:
@@ -24,17 +24,17 @@ def calc_kinetic_energy(m: float, v: Vector) -> float:
     return (v.length()**2)*m/2
 
 
-def calc_collision_1_d_v1(m1: float, m2: float, v1: Vector, v2: Vector, cr: float) -> float:
+def calc_collision_1d_v1(m1: float, m2: float, v1: Vector, v2: Vector, cr: float) -> float:
     return (cr*m2*(v2-v1)+m1*v1+m2*v2)/(m1+m2)
 
 
-def calc_collision_v1(m1: float, m2: float, p1: Vector, p2: Vector, v1: Vector, v2: Vector, cr: float) -> Vector:
-    un = (p1-p2).norm()
+def calc_collision_v1(m1: float, m2: float, pos1: Vector, pos2: Vector, v1: Vector, v2: Vector, cr: float) -> Vector:
+    un = (pos1-pos2).norm()
     v1n = v1.projection_on(un)
     v2n = v2.projection_on(un)
     v1n_len = v1n.length()
     v2n_len = v2n.length()
-    v1n_len_last = calc_collision_1_d_v1(m1, m2, v1n_len, v2n_len, cr)
+    v1n_len_last = calc_collision_1d_v1(m1, m2, v1n_len, v2n_len, cr)
     v1n_last = un*v1n_len_last
     v1t_last = v1-v1n
     v1_last = v1n_last + v1t_last
@@ -45,5 +45,5 @@ def calc_gravity(m1: float, m2: float, d: float) -> float:
     return g*m1*m2/(d**2)
 
 
-def calc_gravity_force1(m1: float, m2: float, p1: Vector, p2: Vector) -> Vector:
-    return (p2-p1).scale_to(calc_gravity(m1, m2, (p2-p1).length()))
+def calc_gravity_force_on_first_object(m1: float, m2: float, pos1: Vector, pos2: Vector) -> Vector:
+    return (pos2-pos1).scale_to(calc_gravity(m1, m2, (pos2-pos1).length()))
--- vector.py
+++ vector.py
@@ -1,31 +1,54 @@
 from __future__ import annotations
 from math import *
+import string
 from numpy import *
+from vpython import *
 
 
 class Vector:
 
-    def __init__(self, x, y, z,):
+    def __init__(self, x, y, z,) -> Vector:
         self.x = x
         self.y = y
         self.z = z
 
-    def __str__(self):
+    def __str__(self) -> string:
         return f'[{self.x} , {self.y} , {self.z}]'
 
-    def __eq__(self, other):
+    def __eq__(self, other: Vector) -> bool:
         return self.x == other.x and self.y == other.y and self.z == other.z
 
-    def __add__(self, other):
+    def __ne__(self, other: Vector) -> bool:
+        return self.x != other.x or self.y != other.y or self.z != other.z
+
+    def __add__(self, other: Vector) -> Vector:
         return Vector(self.x+other.x, self.y+other.y, self.z+other.z)
 
-    def __sub__(self, other):
+    def __iadd__(self, other: Vector) -> Vector:
+        return Vector(self.x+other.x, self.y+other.y, self.z+other.z)
+
+    def __sub__(self, other: Vector) -> Vector:
         return Vector(self.x-other.x, self.y-other.y, self.z-other.z)
 
-    def __mul__(self, other):
+    def __isub__(self, other: Vector) -> Vector:
+        return Vector(self.x-other.x, self.y-other.y, self.z-other.z)
+
+    def __mul__(self, other: float) -> Vector:
         return Vector(self.x*other, self.y*other, self.z*other)
 
-    def __truediv__(self, other):
+    def __rmul__(self, other: float) -> Vector:
+        return Vector(self.x*other, self.y*other, self.z*other)
+
+    def __imul__(self, other: float) -> Vector:
+        return Vector(self.x*other, self.y*other, self.z*other)
+
+    def __truediv__(self, other: float) -> Vector:
+        return Vector(self.x/other, self.y/other, self.z/other)
+
+    def __rtruediv__(self, other: float) -> Vector:
+        return Vector(self.x/other, self.y/other, self.z/other)
+
+    def __itruediv__(self, other: float) -> Vector:
         return Vector(self.x/other, self.y/other, self.z/other)
 
     def dot(self, other: Vector) -> float:
@@ -52,3 +75,6 @@ class Vector:
 
     def projection_length_on(self, other: Vector) -> float:
         return other.dot(self)/other.length()
+
+    def to_vpython_vector(self) -> vector:
+        return vector(self.x, self.y, self.z)
