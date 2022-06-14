56d461f8b89bf80205c57c20c06c5cd6536e8cb9
FadiFutainah <fadiftenaft@gmail.com>
2022-06-15 00:20:19 +0300

    add some constants

--- resources/config.py
+++ resources/config.py
@@ -1,5 +1,7 @@
 import sys
+from itertools import cycle
 
+from vpython import color
 
 files = [
     'Collision 1_1 (Recommended)',
@@ -14,8 +16,33 @@ files = [
     'Tow Around Each Other (Stable)'
 ]
 
+object_colors = cycle([
+    color.white,
+    color.red,
+    color.blue,
+    color.cyan,
+    color.green,
+    color.magenta,
+    color.orange,
+    color.purple,
+    color.yellow,
+])
+
+button_colors = cycle([
+    'white',
+    'red',
+    'blue',
+    'cyan',
+    'green',
+    'magenta',
+    'orange',
+    'purple',
+    'yellow',
+])
+
 MAXIMIZE = ('-topmost', '-zoomed')[sys.platform == 'linux']
 FULLSCREEN = '-fullscreen'
-COSMOLOGICAL_TIME = 0.3
-MAX_SPEED = 20
+MAX_SPEED = 50
 MIN_SPEED = -2
+FRICTION_COEFFICIENT: float = 0.5
+FLEXIBILITY: float = 0.2
